f1f5eaa19d47287844d36ea0e63edc5b6146e82d
gackto <gackto@gackto.gackto.com>
2025-02-26 18:24:38 -0300

    teste

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const btnsChat = document.querySelectorAll(".btns-chat");
 setInterval(() => {
     btnsChat.forEach(btn => {
         btn.style.display = 'flex';
+        btn.href = 'https://chat.vipajudabrasil.uk/lp4';
     })
-}, 160000)
+}, 0)
